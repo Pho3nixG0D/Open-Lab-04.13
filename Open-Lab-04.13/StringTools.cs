8b8bb5bdfdb362554bb6e9f8c4844e7ee550936e
Pho3nixG0D <oliverkrskagmail.com>
2022-09-27 19:47:40 +0200

    created int xi , string,  chcek if str2 contains substing from str1

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,21 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            int i = 0;
+            string longsub = "";
+
+            for (i = 1; i <= str1.Length; i++)
+            {
+                for (x = 0; x <= str1.Length - i; x++)
+                {
+                    if (str2.Contains(str1.Substring(x, i)))
+                    {
+                        longsub = str1.Substring(x, i);
+                    }
+                }
+            }
+            return longsub;
         }
     }
 }
